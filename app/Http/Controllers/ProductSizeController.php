1886eeae48b6a594c4ab17e52e315a16a356e3e2
NguyenVanThiet15 <thiet150879@gmail.com>
2024-10-04 23:25:47 +0700

    fix: resolve conflict

--- app/Http/Controllers/ProductSizeController.php
+++ app/Http/Controllers/ProductSizeController.php
@@ -2,13 +2,9 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
 use App\Models\ProductSize;
 use Illuminate\Http\Request;
-=======
 use App\Http\Requests\ProductSizeRequest;
-use App\Models\ProductSize;
->>>>>>> 4babb3bbeedede67b1ee8a0e1a8e338277a9f568
 
 class ProductSizeController extends Controller
 {
@@ -17,12 +13,8 @@ class ProductSizeController extends Controller
      */
     public function index()
     {
-<<<<<<< HEAD
-        //
-=======
         $productSizes = ProductSize::query()->get();
         return view('admin.productsizes.index', compact('productSizes'));
->>>>>>> 4babb3bbeedede67b1ee8a0e1a8e338277a9f568
     }
 
     /**
@@ -30,59 +22,30 @@ class ProductSizeController extends Controller
      */
     public function create()
     {
-<<<<<<< HEAD
-        //
-=======
         return view('admin.productsizes.create');
->>>>>>> 4babb3bbeedede67b1ee8a0e1a8e338277a9f568
     }
 
     /**
      * Store a newly created resource in storage.
      */
-<<<<<<< HEAD
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(ProductSize $productSize)
-    {
-        //
-=======
     public function store(ProductSizeRequest $request)
     {
         $productSizes = ProductSize::create($request->all());
         return redirect()->route('dashboard.size.index');
->>>>>>> 4babb3bbeedede67b1ee8a0e1a8e338277a9f568
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-<<<<<<< HEAD
-    public function edit(ProductSize $productSize)
-    {
-        //
-=======
     public function edit($id)
     {
         $productSize = ProductSize::find($id);
         return view('admin.productsizes.edit', compact('productSize'));
->>>>>>> 4babb3bbeedede67b1ee8a0e1a8e338277a9f568
     }
 
     /**
      * Update the specified resource in storage.
      */
-<<<<<<< HEAD
-    public function update(Request $request, ProductSize $productSize)
-    {
-        //
-=======
     public function update(ProductSizeRequest $request, $id)
     {
         $productSize = ProductSize::find($id);
@@ -90,22 +53,15 @@ class ProductSizeController extends Controller
             'name' => $request->name
         ]);
         return redirect()->route('dashboard.size.index');
->>>>>>> 4babb3bbeedede67b1ee8a0e1a8e338277a9f568
     }
 
     /**
      * Remove the specified resource from storage.
      */
-<<<<<<< HEAD
-    public function destroy(ProductSize $productSize)
-    {
-        //
-=======
     public function destroy($id)
     {
         $productSizes = ProductSize::find($id);
         $productSizes->delete();
         return redirect()->route('dashboard.size.index');
->>>>>>> 4babb3bbeedede67b1ee8a0e1a8e338277a9f568
     }
 }
--- app/Models/ProductSize.php
+++ app/Models/ProductSize.php
@@ -8,11 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class ProductSize extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
-=======
     protected $table = 'product_sizes';
     protected $primaryKey = 'id';
     public $timestamps = true;
     protected $fillable = ['id', 'name', 'created_at', 'updated_at'];
->>>>>>> 4babb3bbeedede67b1ee8a0e1a8e338277a9f568
 }
