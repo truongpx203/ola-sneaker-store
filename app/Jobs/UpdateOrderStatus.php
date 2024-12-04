095013f869f9833197575fc807ab04b1bc2a0aac
truongpx203 <truong5122003@gmail.com>
2024-12-04 13:15:42 +0700

    Công điểm khi đơn hàng tự động hoàn thành và hoàn lại số lượng tồn kho khi hủy đơn

--- app/Jobs/UpdateOrderStatus.php
+++ app/Jobs/UpdateOrderStatus.php
@@ -22,29 +22,18 @@ class UpdateOrderStatus implements ShouldQueue
 
     public function handle(): void
     {
-        try {
-            // Tìm hóa đơn
-            $bill = Bill::find($this->billId);
-    
-            if ($bill && $bill->bill_status === 'delivered') {
-                $oldStatus = $bill->bill_status;
+        $bill = Bill::find($this->billId);
 
-                $bill->bill_status = 'completed';
-                $bill->save();
-
-                $bill->histories()->create([
-                    'from_status' => $oldStatus,
-                    'to_status' => $bill->bill_status,
-                    'note' => 'Tự động cập nhật từ giao hàng thành công sang hoàn thành',
-                    'by_user' => null,
-                    'at_datetime' => now(),
-                ]);
-    
-                \Log::info("Bill ID {$this->billId} đã được cập nhật trạng thái từ 'delivered' sang 'completed'.");
-            }
-        } catch (\Exception $e) {
-            // Ghi lại lỗi vào log
-            \Log::error('Job UpdateOrderStatus failed: ' . $e->getMessage());
+        if (!$bill || $bill->bill_status !== 'delivered') {
+            return; 
         }
+
+        $bill->bill_status = 'completed';
+        $bill->save();
+
+ 
+        $bill->awardPointsToUser();
+        // $customerEmail = $bill->user->email;
+        // Mail::to($customerEmail)->send(new OrderCompletedMail($bill));
     }
 }
